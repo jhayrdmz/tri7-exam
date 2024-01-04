6a9f924367c96c8eae74e5ef453a950b4230802e
Mario Hugo Jr <mario.hugo@lanexcorp.com>
2024-01-05 01:57:12 +0800

    change angular version for compatibility

--- tri7-exam-frontend/src/app/app.component.spec.ts
+++ tri7-exam-frontend/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tri7-exam-frontend' title`, () => {
+  it(`should have as title 'tri7-exam-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('tri7-exam-frontend');
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tri7-exam-frontend');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tri7-exam-frontend app is running!');
   });
 });
